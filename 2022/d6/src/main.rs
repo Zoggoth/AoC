b43179991d13bded2f8ecfd9f7f3fa6c283c81b9
Zoggoth <luke.ramsden399@gmail.com>
2022-12-06 22:23:17 +0000

    various ways to solve day 6

--- 2022/d6/src/main.rs
+++ 2022/d6/src/main.rs
@@ -1,3 +1,5 @@
+use fancy_regex::Regex;
+
 fn main() {
     let input = std::fs::read_to_string("input.txt").expect("No File");
     let mut i = 0;
@@ -17,17 +19,59 @@ fn main() {
     }
     println!("{:?}",i+4);
     i = 0;
+    let now = std::time::Instant::now();
+    // loop{
+    //     let mut jump = 0;
+    //     for x in (0..14).rev(){
+    //         for y in x+1..14{
+    //             if input.chars().nth(i+x).unwrap() == input.chars().nth(i+y).unwrap(){
+    //                 jump = x+1;
+    //                 break;
+    //             }
+    //         }
+    //         if jump != 0{
+    //             break;
+    //         }
+    //     }
+    //     if jump == 0{
+    //         break;
+    //     }
+    //     else{
+    //         i += jump;
+    //     }
+    // }
+    // loop{
+    //     let mut jump = 0;
+    //     let mut already = std::collections::HashSet::new();
+    //     already.insert(input.chars().nth(i+14).unwrap());
+    //     for x in (0..14).rev(){
+    //         if already.contains(&input.chars().nth(i+x).unwrap()){
+    //             jump = x;
+    //             break;
+    //         }
+    //         else{
+    //             already.insert(input.chars().nth(i+x).unwrap());
+    //         }
+    //     }
+    //     if jump == 0{
+    //         break;
+    //     }
+    //     else{
+    //         i += jump;
+    //     }
+    // }
+    let new_input: &[u8]= input.as_ref();
     loop{
         let mut jump = 0;
+        let mut already = bit_set::BitSet::with_capacity(26);
+        already.insert((new_input[i+14] - 97) as usize);
         for x in (0..14).rev(){
-            for y in x+1..14{
-                if input.chars().nth(i+x).unwrap() == input.chars().nth(i+y).unwrap(){
-                    jump = x+1;
-                    break;
-                }
-            }
-            if jump != 0{
+            if already.contains((new_input[i + x] - 97) as usize){
+                jump = x;
                 break;
+            }
+            else{
+                already.insert((new_input[i + x] - 97) as usize);
             }
         }
         if jump == 0{
@@ -37,5 +81,15 @@ fn main() {
             i += jump;
         }
     }
+    // let mut regex_text : String = "(.)".to_string();
+    // let mut regex_part : String = "".to_string();
+    // for i in 1..14{
+    //     regex_part.push_str(&*("(?!\\".to_owned() + &*i.to_string() + ")"));
+    //     regex_text.push_str(&*(regex_part.to_owned() + "(.)"));
+    // }
+    // let re = Regex::new(&*regex_text).unwrap();
+    // i = re.find(&input).unwrap().unwrap().start();
+    let elapsed = now.elapsed();
     println!("{:?}",i+14);
+    println!("Elapsed: {:.2?}", elapsed);
 }
