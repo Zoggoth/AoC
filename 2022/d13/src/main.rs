19469dec9319e886851a425425aa85be849c00b5
Zoggoth <luke.ramsden399@gmail.com>
2022-12-13 18:31:18 +0000

    struct -> enum

--- 2022/d13/src/main.rs
+++ 2022/d13/src/main.rs
@@ -3,12 +3,13 @@ extern crate core;
 use std::fs::read_to_string;
 use std::cmp::{min, Ordering};
 use std::cmp::Ordering::{Equal, Greater, Less};
+use crate::Packet::{List, Value};
 
 #[derive(Debug)]
-#[derive(Clone)]
-struct Packet {
-    value: Option<i32>,
-    list: Option<Vec<Packet>>
+enum Packet {
+    Value(i32),
+    List(Vec<Packet>),
+    None
 }
 
 impl Eq for Packet {}
@@ -27,61 +28,39 @@ impl PartialOrd<Self> for Packet {
 
 impl Ord for Packet {
     fn cmp(&self, other: &Self) -> Ordering {
-        let self_is_none = self.list.is_none() & self.value.is_none();
-        let other_is_none = other.list.is_none() & other.value.is_none();
-        if self_is_none & other_is_none{
-            return Equal;
-        }
-        if self_is_none{
-            return Less;
-        }
-        if other_is_none{
-            return Greater;
-        }
-        if self.value.is_some() & other.value.is_some(){
-            if self.value.unwrap() < other.value.unwrap(){
-                return Less;
+        return match (self, other) {
+            (Packet::None, Packet::None) => { Equal }
+            (Packet::None, _) => { Less }
+            (_, Packet::None) => { Greater }
+            (Value(x), Value(y)) => { x.cmp(y) }
+            (Value(x), y@ List(_)) => { List(vec![Value(*x)]).cmp(y) }
+            (x@ List(_), Value(y)) => { x.cmp(&List(vec![Value(*y)])) }
+            (List(x), List(y)) => {
+                let zmax = min(x.len(),y.len());
+                for z in 0..zmax{
+                    let self_z = &x[z];
+                    let other_z = &y[z];
+                    match self_z.cmp(other_z){
+                        Less => {return Less;}
+                        Greater => {return Greater;}
+                        Equal => {}
+                    };
+                }
+                if x.len() < y.len(){
+                    return Less;
+                }
+                if x.len() > y.len(){
+                    return Greater;
+                }
+                Equal
             }
-            if self.value.unwrap() > other.value.unwrap(){
-                return Greater;
-            }
-            return Equal;
         }
-        let self_list = if self.value.is_some() {
-            vec![Packet { value: self.value, list: None }]
-        }
-        else{
-            self.list.clone().unwrap()
-        };
-        let other_list = if other.value.is_some() {
-            vec![Packet { value: other.value, list: None }]
-        }
-        else{
-            other.list.clone().unwrap()
-        };
-        let xmax = min(self_list.len(),other_list.len());
-        for x in 0..xmax{
-            let self_x = &self_list[x];
-            let other_x = &other_list[x];
-            match self_x.cmp(other_x){
-                Less => {return Less;}
-                Greater => {return Greater;}
-                Equal => {}
-            };
-        }
-        if self_list.len() < other_list.len(){
-            return Less;
-        }
-        if self_list.len() > other_list.len(){
-            return Greater;
-        }
-        return Equal;
     }
 }
 
 fn parse(description: &str) -> Packet {
     return if description == ""{
-        return Packet { value: None, list: None }
+        Packet::None
     }
     else if description[0..1] == *"[" {
         let mut items: Vec<&str> = vec![];
@@ -107,10 +86,10 @@ fn parse(description: &str) -> Packet {
         for x in items {
             list.push(parse(x));
         }
-        let output = Packet { value: None, list: Some(list) };
+        let output = Packet::List(list);
         output
     } else {
-        let output = Packet { value: Some(description.parse::<i32>().unwrap()), list: None };
+        let output = Packet::Value(description.parse::<i32>().unwrap());
         output
     }
 }
