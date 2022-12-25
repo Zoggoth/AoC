4ec5b3b18c68ef24ff3a45cc6fdbdea77968bce1
Zoggoth <luke.ramsden399@gmail.com>
2022-12-25 13:59:04 +0000

    divmod together

--- 2022/d25/src/main.rs
+++ 2022/d25/src/main.rs
@@ -20,15 +20,17 @@ fn main() {
     }
     let mut current = "".to_string();
     while part1 != 0{
-        match part1 % 5 {
-            0 => {current = "0".to_string() + &current}
-            1 => {current = "1".to_string() + &current}
-            2 => {current = "2".to_string() + &current}
-            3 => {current = "=".to_string() + &current}
-            4 => {current = "-".to_string() + &current}
+        part1 += 2;
+        let res = (part1/5, part1 % 5);
+        match res.1 {
+            0 => {current = "=".to_string() + &current}
+            1 => {current = "-".to_string() + &current}
+            2 => {current = "0".to_string() + &current}
+            3 => {current = "1".to_string() + &current}
+            4 => {current = "2".to_string() + &current}
             _ => {}
         }
-        part1 = (part1 + 2)/5
+        part1 = res.0
     }
     println!("{}",current);
 }
